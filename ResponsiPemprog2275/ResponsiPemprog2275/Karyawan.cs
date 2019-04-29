0dd1506ac91f413225fed6f3c97a66ea1004f513
habibrifqi <habib.rifqi.16@gmail.com>
2019-04-29 14:46:00 +0700

    responsi

---
+++ ResponsiPemprog2275/ResponsiPemprog2275/Karyawan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemprog2275
+{
+    public class Karyawan
+    {
+        public Karyawan()
+        {
+
+        }
+
+        public Karyawan(string Nik, string Nama, int Gajibulanan)
+        {
+            if (Gajibulanan < 0)
+            {
+                Console.WriteLine ("gaji harus di atas 0");
+            }
+            else
+            {
+                nik = Nik;
+                nama = Nama;
+                gajibulanan = Gajibulanan;
+            }
+        }
+
+        public string nik { get; set; }
+        public string nama { get; set; }
+        public int gajibulanan { get; set; }
+
+    }
+}
---
+++ ResponsiPemprog2275/ResponsiPemprog2275/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemprog2275
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan kry1 = new Karyawan("1234567","habib",2000000);
+
+
+            Karyawan kry2 = new Karyawan("1234567","rifqi",1000000);
+
+
+            Console.WriteLine("No NIK/NAMA \t\tGajibulanan");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("1.{0} {1}\t\t{2}", kry1.nik, kry1.nama, kry1.gajibulanan);
+            Console.WriteLine("2.{0} {1}\t\t{2}", kry2.nik, kry2.nama, kry2.gajibulanan);
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("asik naik gaji 10%");
+            Console.WriteLine("1.{0} {1}\t\t{2}", kry1.nik, kry1.nama, kry1.gajibulanan +(kry1.gajibulanan * 0.10));
+            Console.WriteLine("2.{0} {1}\t\t{2}", kry2.nik, kry2.nama, kry2.gajibulanan + (kry2.gajibulanan * 0.10));
+            Console.ReadKey();
+
+
+
+        }
+
+
+    }
+}
